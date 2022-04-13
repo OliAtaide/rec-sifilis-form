d58da6868ad961da7241afcb03e9e87e1ad68792
Filipe de Oliveira Ataíde <filipe.ataide@academico.ifrn.edu.br>
2022-04-13 13:48:00 -0300

    Alterando arquivos

---
+++ script.js
@@ -0,0 +1,10 @@
+function format(num) {
+    return ("0" + num).slice(-2);
+}
+
+const d = new Date();
+var time = format(d.getHours()) + ':' + format(d.getMinutes());
+var date = format(d.getDate()) + '/' + format(d.getMonth()) + '/' + d.getFullYear();
+$('.date').append(
+    time + '<br>' + date
+);
